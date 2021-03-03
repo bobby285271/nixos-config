f468f25e0f04222c23c498be36ecf0f8d160835a
Bobby Rong <rjl931189261@126.com>
2021-03-03 21:54:05 +0800

    update configuration

--- sys/system.nix
+++ sys/system.nix
@@ -6,6 +6,18 @@
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "peepcode";
+      plugins = [ "git" "python" "man" "fzf"];
+    };
+  };
+  environment.shells = [ pkgs.bashInteractive pkgs.zsh ];
+  programs.thefuck.enable = true;
+
   services.tlp.enable = true;
 
   programs.bash.enableCompletion = true;
