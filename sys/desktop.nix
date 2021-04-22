2f337c3072a519238291cc45e908d85b093bfcb1
Bobby Rong <rjl931189261@126.com>
2021-04-22 10:57:39 +0800

    remove some unused pkg

--- sys/desktop.nix
+++ sys/desktop.nix
@@ -7,4 +7,16 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome3.enable = true;  
   };
+
+  environment.gnome3.excludePackages = with pkgs; [
+    epiphany
+    gnome3.cheese
+		gnome3.gnome-contacts
+		gnome3.gnome-maps
+		gnome3.simple-scan
+		gnome3.yelp
+		gnome-connections
+		gnome-photos
+	];
+
 }
