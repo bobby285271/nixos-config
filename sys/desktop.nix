4abdd6bdc8a9b95f5bea3a75f018fef827b0151b
Bobby Rong <rjl931189261@126.com>
2021-05-09 15:32:09 +0800

    switch to plasma5

--- sys/desktop.nix
+++ sys/desktop.nix
@@ -4,18 +4,7 @@
   services.xserver = {
     enable = true;
     layout = "us";
-    displayManager.gdm.enable = true;
-    desktopManager.gnome3.enable = true;  
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
   };
-
-  environment.gnome3.excludePackages = with pkgs; [
-    epiphany
-    gnome3.cheese
-    gnome3.gnome-contacts
-    gnome3.gnome-maps
-    gnome3.simple-scan
-    gnome3.yelp
-    gnome-connections
-    gnome-photos
-  ];
 }
