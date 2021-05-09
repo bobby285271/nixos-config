303e5d963dc088761334e095c1e3e7d945b01df9
Bobby Rong <rjl931189261@126.com>
2021-05-09 15:32:20 +0800

    add hashedPassword for bobby285271

--- sys/users.nix
+++ sys/users.nix
@@ -3,9 +3,10 @@
 {
   users.users.bobby285271 = { 
     isNormalUser = true;
+    hashedPassword = "$6$0MOOo3w4xnwaTK07$us6355WMS/QG.TDsY1qfiK3vhSEQo1pfsx0xEWzLMD/9A/mOty889pr5XGFGYOeJoNbVLCk/DgBxq7g5JPJsh1";
     home = "/home/bobby285271";
     description = "Bobby Rong";
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    extraGroups = [ "wheel" "networkmanager" "docker" "audio" "sound" "video" "networkmanager" "input" "tty" ];
   };
 
   security.sudo.extraRules = [
