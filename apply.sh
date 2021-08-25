4f620345d3284685f2de8ab8302fc2fdd9cbd265
Bobby Rong <rjl931189261@126.com>
2021-08-25 16:52:21 +0800

    update apply script

--- apply.sh
+++ apply.sh
@@ -6,5 +6,9 @@ then
     sudo rm -rf /etc/nixos/*
     sudo cp -r ${DIR}/* /etc/nixos
 fi
-#sudo nixos-rebuild switch --upgrade
-sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild boot
+
+if [ "$#" -ne 1 ]; then
+    sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild boot
+else
+    sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild ${1}
+fi
