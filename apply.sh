94154b2d8e790f7f4ca9d033bf37b7f960db1d56
Bobby Rong <rjl931189261@126.com>
2021-09-01 22:48:47 +0800

    pass all args in apply script

--- apply.sh
+++ apply.sh
@@ -7,8 +7,8 @@ then
     sudo cp -r ${DIR}/* /etc/nixos
 fi
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -eq 0 ]; then
     sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild boot
 else
-    sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild ${1}
+    sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild $*
 fi
