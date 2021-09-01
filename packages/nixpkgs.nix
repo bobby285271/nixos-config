0ec3d6fa99855302fece3959c4e2422b6405eeee
Bobby Rong <rjl931189261@126.com>
2021-09-01 23:01:21 +0800

    add ustc mirror

--- packages/nixpkgs.nix
+++ packages/nixpkgs.nix
@@ -5,6 +5,7 @@
     binaryCaches = [ 
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
     ];
   };
 
