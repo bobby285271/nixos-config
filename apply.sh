4d4a6121d0ab8e67a3750cc56b2e7baad64eb56b
Bobby Rong <rjl931189261@126.com>
2021-10-06 21:57:50 +0800

    update apply script

--- apply.sh
+++ apply.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
-#sudo nix-collect-garbage -d
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [[ ${DIR} != "/etc/nixos" ]]
+
+CURRENT_DIR="$(pwd)"
+PROFILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NIXPKGS_DIR="/home/bobby285271/nixpkgs"
+REBUILD_CMD="sudo env NIX_PATH=nixpkgs=${NIXPKGS_DIR}:nixos-config=/etc/nixos/configuration.nix nixos-rebuild"
+
+cd ${NIXPKGS_DIR}
+echo -e "\033[33m================================================================\033[0m"
+echo "Current NixOS version: $(nixos-version)"
+echo "Target branch:         $(git branch --show-current)"
+echo -e "\033[33m================================================================\033[0m"
+echo "$(git log -3)"
+echo -e "\033[33m================================================================\033[0m"
+cd ${CURRENT_DIR}
+read 
+
+if [[ ${PROFILE_DIR} != "/etc/nixos" ]]
 then
     sudo rm -rf /etc/nixos/*
-    sudo cp -r ${DIR}/* /etc/nixos
+    sudo cp -r ${PROFILE_DIR}/* /etc/nixos
 fi
 
 if [ "$#" -eq 0 ]; then
-    sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild boot
+    ${REBUILD_CMD} boot
 else
-    sudo env NIX_PATH=nixpkgs=$HOME/nixpkgs:nixos-config=/etc/nixos/configuration.nix nixos-rebuild $*
+    ${REBUILD_CMD} $*
 fi
