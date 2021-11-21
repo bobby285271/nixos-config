12339066d3cc141eb0781beb2d3e132c12f1a3fd
Bobby Rong <rjl931189261@126.com>
2021-11-21 21:58:52 +0800

    prefer git+file:///home/bobby285271/nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nixos-bobby285271";
 
   # Use the following when preparing Pantheon updates:
-  # nix flake update --override-input nixpkgs path:/home/bobby285271/nixpkgs
+  # nix flake update --override-input nixpkgs git+file:///home/bobby285271/nixpkgs
   inputs.nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-unstable";
 
   outputs = { self, nixpkgs }: {
