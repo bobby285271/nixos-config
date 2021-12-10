d8c638ddec0425a233130a1ce53897636646e2c0
Bobby Rong <rjl931189261@126.com>
2021-12-10 15:59:00 +0800

    add bobby285271 cachix

--- system/nixconfig.nix
+++ system/nixconfig.nix
@@ -7,10 +7,16 @@
       auto-optimise-store = true
       flake-registry = https://cdn.jsdelivr.net/gh/NixOS/flake-registry/flake-registry.json
     '';
+
     binaryCaches = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://bobby285271.cachix.org"
+    ];
+
+    binaryCachePublicKeys = [
+      "bobby285271.cachix.org-1:suZrwf/wphZbNXocjgcc49lcn/RB0iFxlh1peSVOE3w="
     ];
 
     registry.nixpkgs = {
