cf7ae71df520a34a426d8b9b8ec55807a186fe11
Bobby Rong <rjl931189261@126.com>
2021-12-29 18:52:31 +0800

    merge boot config

--- machines/inspiron/boot.nix
+++ machines/inspiron/boot.nix
@@ -2,6 +2,18 @@
 
 {
   boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    plymouth.enable = true;
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = true;
+        efiSupport = true;
+      };
+    };
+
     extraModprobeConfig = ''
       options bbswitch load_state=0 unload_state=1
     '';
