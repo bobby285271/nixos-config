0281aa16d54a18cd8bec324b376f0b5ea6168fef
Bobby Rong <rjl931189261@126.com>
2022-01-20 09:59:15 +0800

    update git config

--- programs/git.nix
+++ programs/git.nix
@@ -5,9 +5,6 @@
     enable = true;
     config = {
       init.defaultBranch = "master";
-      url."https://github.com.cnpmjs.org/".insteadOf = [
-        "https://github.com/"
-      ];
       core.compression = 0;
       http.postBuffer = 1048576000;
       protocol."https".allow = "always";
