0ba22fc4aebeb65f5b961c27ddfb1f8d5d3b8132
Bobby Rong <rjl931189261@126.com>
2022-02-05 20:30:54 +0800

    update substituters

--- system/nixconfig.nix
+++ system/nixconfig.nix
@@ -11,8 +11,8 @@
     substituters = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://bobby285271.cachix.org"
+      "https://cache.nixos.org"
     ];
     trusted-public-keys = [
       "bobby285271.cachix.org-1:suZrwf/wphZbNXocjgcc49lcn/RB0iFxlh1peSVOE3w="
