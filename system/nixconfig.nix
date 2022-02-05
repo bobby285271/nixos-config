20b7b0211a85b13c3776c0b36f7f7121947a3e5e
Bobby Rong <rjl931189261@126.com>
2022-02-05 20:34:00 +0800

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'git+file:///home/bobby285271/nixpkgs?ref=nixos-unstable&rev=554d2d8aa25b6e583575459c297ec23750adb6cb' (2022-02-02)
      → 'git+file:///home/bobby285271/nixpkgs?ref=fcitx&rev=d28495a8614a0433e5a2593ce67baad550a89af7' (2022-02-03)

--- system/nixconfig.nix
+++ system/nixconfig.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   nix.settings = {
@@ -8,11 +8,10 @@
     ];
     auto-optimise-store = true;
     flake-registry = "https://cdn.jsdelivr.net/gh/NixOS/flake-registry/flake-registry.json";
-    substituters = [
+    substituters = lib.mkBefore [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://bobby285271.cachix.org"
-      "https://cache.nixos.org"
     ];
     trusted-public-keys = [
       "bobby285271.cachix.org-1:suZrwf/wphZbNXocjgcc49lcn/RB0iFxlh1peSVOE3w="
