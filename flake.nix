15987620aba2d799dad2efb5175cf8a82f719df8
Bobby Rong <rjl931189261@126.com>
2022-03-22 10:22:30 +0800

    pass on the flake inputs to external configuration files

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs.nixpkgs.url = "git+file:///home/bobby285271/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }@attrs: {
     nixosConfigurations = {
       inspiron = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = attrs;
         modules = [ ./profiles/inspiron.nix ];
       };
     };
