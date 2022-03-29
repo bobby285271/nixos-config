c60669c52b68b25a264cfe66c3003b7dd9620a2f
Bobby Rong <rjl931189261@126.com>
2022-03-29 16:56:34 +0800

    Revert "try extraGSettingsOverrides"
    This reverts commit dfa119f9bd695da88099cc2f3029bbf9ca1cce0b.

--- desktop/pantheon.nix
+++ desktop/pantheon.nix
@@ -10,14 +10,4 @@
       extraSwitchboardPlugs = [ pkgs.pantheon-tweaks ];
     };
   };
-
-  services.xserver.desktopManager.pantheon.extraGSettingsOverrides = ''
-    # These paths are supposed to refer to gnome-backgrounds
-    # but since we do not use FHS, they are broken.
-    # And we do not want to hardcode the correct paths
-    # since then every GTK app would pull in gnome-backgrounds.
-    # Let’s just override the broken paths so that people are not confused.
-    [org.gnome.desktop.background]
-    picture-uri-dark='''
-  '';
 }
