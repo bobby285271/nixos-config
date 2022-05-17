e66c9880d900614b262e58a8449eca11d7514e89
Bobby Rong <rjl931189261@126.com>
2022-05-17 15:51:34 +0800

    docker: set registry-mirrors

--- programs/docker.nix
+++ programs/docker.nix
@@ -1,3 +1,10 @@
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      "registry-mirrors" = [
+        "https://bobm60dq.mirror.aliyuncs.com"
+      ];
+    };
+  };
 }
