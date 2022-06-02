38079b0a38d3b765bb063c6f4569733d85637172
Bobby Rong <rjl931189261@126.com>
2022-06-02 13:23:19 +0800

    simplify substituters

--- system/nixconfig.nix
+++ system/nixconfig.nix
@@ -9,13 +9,12 @@
     auto-optimise-store = true;
     flake-registry = "https://gitee.com/bobby285271/flake-registry/raw/master/flake-registry.json";
     substituters = lib.mkBefore [
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://bobby285271.cachix.org"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      # "https://bobby285271.cachix.org"
     ];
-    trusted-public-keys = [
-      "bobby285271.cachix.org-1:suZrwf/wphZbNXocjgcc49lcn/RB0iFxlh1peSVOE3w="
-    ];
+    # trusted-public-keys = [
+    #   "bobby285271.cachix.org-1:suZrwf/wphZbNXocjgcc49lcn/RB0iFxlh1peSVOE3w="
+    # ];
   };
 
   nixpkgs.config = {
