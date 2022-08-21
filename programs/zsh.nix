cb47b57664d5515838b91b2a4b110e0de649337e
Bobby Rong <rjl931189261@126.com>
2022-08-21 19:24:49 +0800

    Revert "fish: switch from zsh"
    This reverts commit bccafd8730ecca14746b79821b3c130dd4b9805b.

---
+++ programs/zsh.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  programs = {
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      ohMyZsh = {
+        enable = true;
+        theme = "ys";
+        plugins = [
+          "git"
+          "python"
+          "man"
+        ];
+      };
+    };
+    thefuck.enable = true;
+    bash.enableCompletion = true;
+  };
+
+  environment = {
+    shells = [ pkgs.bashInteractive pkgs.zsh ];
+    shellInit = ''
+      export GPG_TTY="$(tty)"
+    '';
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+}
