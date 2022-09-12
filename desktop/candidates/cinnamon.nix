8377214078eca34bd3eed9eef8f667d061b3ae51
Bobby Rong <rjl931189261@126.com>
2022-09-12 16:29:48 +0800

    cinnamon.nemo: fix duplicate right-click menu items

--- desktop/candidates/cinnamon.nix
+++ desktop/candidates/cinnamon.nix
@@ -15,4 +15,24 @@
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      cinnamon = super.cinnamon.overrideScope' (pself: psuper: {
+        nemo = psuper.nemo.overrideAttrs (oldAttrs: {
+          prePatch = ''
+            for f in \
+              src/nemo-action-config-widget.c \
+              src/nemo-script-config-widget.c \
+              libnemo-private/nemo-action-manager.c
+            do
+              substituteInPlace $f --replace \
+                'g_build_filename (data_dirs[i], "nemo", "actions", NULL)' \
+                'g_build_filename (NEMO_DATADIR, "actions", NULL)'
+            done
+          '';
+        });
+      });
+    })
+  ];
 }
