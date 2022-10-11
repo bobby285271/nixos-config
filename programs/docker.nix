6cb3b41597d7d932e9c6c6fc861a63e666f74641
Bobby Rong <rjl931189261@126.com>
2022-10-11 15:07:46 +0800

    virtualisation: enable libvirtd

--- programs/docker.nix
+++
@@ -1,9 +0,0 @@
-{
-  virtualisation.docker = {
-    enable = true;
-    daemon.settings = {
-      "registry-mirrors" = [ "https://bobm60dq.mirror.aliyuncs.com" ];
-      "live-restore" = false;
-    };
-  };
-}
