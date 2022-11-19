3928f3a2373c96a414bc3367db49434ad293f76e
Bobby Rong <rjl931189261@126.com>
2022-11-19 16:40:31 +0800

    Fix mismatch kernel

--- machines/inspiron/boot.nix
+++ machines/inspiron/boot.nix
@@ -14,7 +14,7 @@
     extraModprobeConfig = ''
       options bbswitch load_state=0 unload_state=1
     '';
-    extraModulePackages = [ pkgs.linuxPackages_latest.bbswitch ];
+    extraModulePackages = [ pkgs.linuxPackages.bbswitch ];
     kernelModules = [ "bbswitch" ];
     blacklistedKernelModules = [ "nouveau" ];
   };
