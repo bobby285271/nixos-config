17ca90febd7da4b71139ab92fdd07dc137f2b841
Bobby Rong <rjl931189261@126.com>
2023-01-02 09:10:49 +0800

    Revert "Revert "switch to nvidia-offload""
    This reverts commit 10476dd60570247d39565ca6351054104a881d39.

--- machines/inspiron/boot.nix
+++ machines/inspiron/boot.nix
@@ -11,12 +11,13 @@
         configurationLimit = 5;
       };
     };
+  };
 
-    extraModprobeConfig = ''
-      options bbswitch load_state=0 unload_state=1
-    '';
-    extraModulePackages = [ pkgs.linuxPackages_latest.bbswitch ];
-    kernelModules = [ "bbswitch" ];
-    blacklistedKernelModules = [ "nouveau" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
   };
 }
