af3676534abfa8e356bf4275f513f59a5af0680a
Bobby Rong <rjl931189261@126.com>
2023-01-10 21:30:26 +0800

    system: re-add tlp

--- machines/inspiron/system.nix
+++ machines/inspiron/system.nix
@@ -1,6 +1,11 @@
 {
   networking.hostName = "inspiron";
 
+  services = {
+    power-profiles-daemon.enable = false;
+    tlp.enable = true;
+  };
+
   security.sudo.extraRules = [{
     users = [ "bobby285271" ];
     commands = [{
