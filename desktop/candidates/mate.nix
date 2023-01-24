20ef251fed17730d9629ab4395e1cb68117af5a0
Bobby Rong <rjl931189261@126.com>
2023-01-24 21:11:30 +0800

    mate: patch

--- desktop/candidates/mate.nix
+++ desktop/candidates/mate.nix
@@ -8,13 +8,39 @@
       desktopManager.mate.enable = true;
       displayManager.lightdm.greeters.slick = {
         enable = true;
-        theme.name = "BlueMenta";
-        iconTheme.name = "mate";
+        theme.name = "Mint-Y-Sand";
+        iconTheme.name = "Mint-Y-Sand";
         draw-user-backgrounds = true;
       };
     };
     flatpak.enable = true;
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      mate = super.mate.overrideScope' (pself: psuper: {
+        mate-terminal = psuper.mate-terminal.overrideAttrs (oldAttrs: {
+          patches = (oldAttrs.patches or [ ]) ++ [
+            # Dark style
+            (super.fetchurl {
+              url = "https://jihulab.com/bobby285271/mate-patches/-/raw/8b43598992aa452dc9045fe4d6bb959a06a8bdbb/mate-terminal-dark-style.patch";
+              hash = "1dj73in8bh90kwcd25hdizbwhljsq9i3g5zzj0iybw59lkvzfkjv";
+            })
+          ];
+        });
+
+        mate-control-center = psuper.mate-control-center.overrideAttrs (oldAttrs: {
+          patches = (oldAttrs.patches or [ ]) ++ [
+            # Dark style
+            (super.fetchurl {
+              url = "https://jihulab.com/bobby285271/mate-patches/-/raw/8b43598992aa452dc9045fe4d6bb959a06a8bdbb/mate-control-center-metacity-3-support.patch";
+              hash = "1nhqa3q53af4fnv8ya5ffa20i9z543av4q86y7ak7f462rgga3zp";
+            })
+          ];
+        });
+      });
+    })
+  ];
 
   xdg.portal = {
     enable = true;
@@ -22,6 +48,8 @@
   };
 
   environment.systemPackages = with pkgs; [
+    cinnamon.mint-themes
+    cinnamon.mint-y-icons
     networkmanagerapplet
   ];
 }
