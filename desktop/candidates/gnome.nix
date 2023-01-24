db0ed1dc0820edfda59024bf01c088fbaf57ce80
Bobby Rong <rjl931189261@126.com>
2023-01-24 21:11:37 +0800

    gnome: clean up

--- desktop/candidates/gnome.nix
+++ desktop/candidates/gnome.nix
@@ -12,21 +12,4 @@
   };
 
   xdg.portal.enable = true;
-
-  nixpkgs.overlays = [
-    (self: super: {
-      gnome = super.gnome.overrideScope' (pself: psuper: {
-        # gnome-terminal = psuper.gnome-terminal.overrideAttrs (oldAttrs: {
-        #   patches = (oldAttrs.patches or [ ]) ++ [
-        #     # Restore transparency
-        #     # https://git.launchpad.net/ubuntu/+source/gnome-terminal/refs/
-        #     (super.fetchpatch {
-        #       url = "https://git.launchpad.net/ubuntu/+source/gnome-terminal/plain/debian/patches/0001-Restore-transparency.patch?h=import/3.44.1-1ubuntu1";
-        #       hash = "sha256-WjQd+IFu0dj1dqoKByWpi9hQFek73ae0NCuwm9YRm4o=";
-        #     })
-        #   ];
-        # });
-      });
-    })
-  ];
 }
