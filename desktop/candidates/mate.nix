8f8dc59faa11213bf5b250348da244d103e72d12
Bobby Rong <rjl931189261@126.com>
2023-01-24 17:26:28 +0800

    mate: updates

--- desktop/candidates/mate.nix
+++ desktop/candidates/mate.nix
@@ -2,17 +2,14 @@
 
 {
   services = {
-    packagekit.enable = true;
     xserver = {
       enable = true;
       layout = "us";
       desktopManager.mate.enable = true;
       displayManager.lightdm.greeters.slick = {
         enable = true;
-        theme.name = "Qogir-Dark";
-        theme.package = pkgs.qogir-theme;
-        iconTheme.name = "Qogir";
-        iconTheme.package = pkgs.qogir-icon-theme;
+        theme.name = "BlueMenta";
+        iconTheme.name = "mate";
         draw-user-backgrounds = true;
       };
     };
@@ -24,15 +21,7 @@
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
-  nixpkgs.overlays = [
-    (self: super: {
-      # Nothing yet!
-    })
-  ];
-
   environment.systemPackages = with pkgs; [
-    qogir-theme
-    qogir-icon-theme
     networkmanagerapplet
   ];
 }
