e3e6bfc0907e66dcded5cceecd177af0cfd4a88f
Bobby Rong <rjl931189261@126.com>
2023-03-11 17:39:44 +0800

    libreoffice-icon-fixed: rewrite

--- desktop/pkgs/libreoffice-icon-fixed.nix
+++ desktop/pkgs/libreoffice-icon-fixed.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs ? import <nixpkgs> {}, ... }:
 
 pkgs.symlinkJoin {
   name = "libreoffice-icon-fixed";
@@ -6,32 +6,31 @@ pkgs.symlinkJoin {
   paths = [ pkgs.libreoffice-fresh ];
 
   postBuild = ''
-    rm -r $out/share/applications
+    rm -rf $out/share $out/lib
+
     TMPDIR=$(mktemp -d)
-    cp -r ${pkgs.libreoffice-fresh}/share/applications/. $TMPDIR
+    cp -r ${pkgs.libreoffice-fresh-unwrapped}/. $TMPDIR
+    chmod -R +w $TMPDIR
 
-    # Is the issue fixed upstream?
-    cat $TMPDIR/calc.desktop | grep 'Icon=calc' > /dev/null
+    cat $TMPDIR/lib/libreoffice/share/xdg/calc.desktop | grep 'Icon=calc' > /dev/null
 
-    find $TMPDIR -type f -exec sed -i \
+    find $TMPDIR/lib/libreoffice/share/xdg -type f -exec sed -i \
       -e s,Icon=,Icon=libreoffice-,g \
       -e s,Icon=libreoffice-document-new,Icon=document-new,g \
       {} +
 
-    cp -r $TMPDIR/. $out/share/applications
+    cat $TMPDIR/lib/libreoffice/share/xdg/calc.desktop | grep 'Icon=libreoffice-calc' > /dev/null
+    cat $TMPDIR/lib/libreoffice/share/xdg/calc.desktop | grep 'Icon=document-new' > /dev/null
 
-    # Is the issue fixed downstream?
-    cat $out/share/applications/calc.desktop | grep 'Icon=libreoffice-calc' > /dev/null
-    cat $out/share/applications/calc.desktop | grep 'Icon=document-new' > /dev/null
+    pushd $TMPDIR/share/icons
+      [ ! -f 'hicolor/32x32/apps/libreoffice-base.png' ]
+      [ -f 'hicolor/32x32/apps/base.png' ]
+      # https://unix.stackexchange.com/questions/310256/for-all-directories-rename-all-subdirectories-with-a-prefix
+      find . -maxdepth 4 -mindepth 4 -type f -execdir bash -c 'cp "$1" "./libreoffice-''${1#./}"' mover {} \;
+      [ -f 'hicolor/32x32/apps/libreoffice-base.png' ]
+    popd
 
-    rm -r $out/share/icons
-    TMPDIR2=$(mktemp -d)
-    cp -r -L ${pkgs.libreoffice-fresh}/share/icons/. $TMPDIR2
-    find $TMPDIR2 -type d -exec chmod 755 {} \;
-
-    # https://unix.stackexchange.com/questions/310256/for-all-directories-rename-all-subdirectories-with-a-prefix
-    find $TMPDIR2 -maxdepth 4 -mindepth 4 -type f -execdir bash -c 'cp "$1" "./libreoffice-''${1#./}"' mover {} \;
-
-    cp -r $TMPDIR2/. $out/share/icons
+    cp -r $TMPDIR/share $out
+    cp -r $TMPDIR/lib $out
   '';
 }
