57817cdbffc3619185b4e02068595e7afbfc7039
Bobby Rong <rjl931189261@126.com>
2023-03-14 19:54:17 +0800

    Revert "iso: upd"
    This reverts commit 3ad3fb08521dd68c5d9bcbc59f93f5811c2cd0e4.

--- machines/iso/default.nix
+++ machines/iso/default.nix
@@ -1,12 +1,5 @@
-{ pkgs, modulesPath, ... }:
+{ config, lib, modulesPath, ... }:
 
 {
   imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix" ];
-
-  users.users.nixos.password = "nixos";
-
-  services.xserver.displayManager.lightdm.extraConfig = ''
-    [SeatDefaults]
-    autologin-user=nixos
-  '';
 }
