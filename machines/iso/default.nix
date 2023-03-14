3ad3fb08521dd68c5d9bcbc59f93f5811c2cd0e4
Bobby Rong <rjl931189261@126.com>
2023-03-14 18:55:11 +0800

    iso: upd

--- machines/iso/default.nix
+++ machines/iso/default.nix
@@ -1,5 +1,12 @@
-{ config, lib, modulesPath, ... }:
+{ pkgs, modulesPath, ... }:
 
 {
   imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-graphical-base.nix" ];
+
+  users.users.nixos.password = "nixos";
+
+  services.xserver.displayManager.lightdm.extraConfig = ''
+    [SeatDefaults]
+    autologin-user=nixos
+  '';
 }
