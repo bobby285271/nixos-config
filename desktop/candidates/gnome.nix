d8113acf2596d34ae1fcbc0f10c69150708714bc
Bobby Rong <rjl931189261@126.com>
2023-04-19 22:47:56 +0800

    gnome: add some debug notes

--- desktop/candidates/gnome.nix
+++ desktop/candidates/gnome.nix
@@ -6,7 +6,7 @@
       enable = true;
       layout = "us";
       desktopManager.gnome.enable = true;
-      displayManager.gdm.enable =  true;
+      displayManager.gdm.enable = true;
     };
     flatpak.enable = true;
     fwupd.enable = true;
@@ -14,4 +14,26 @@
 
   programs.gpaste.enable = true;
   xdg.portal.enable = true;
+
+  # system.replaceRuntimeDependencies =
+  #   let
+  #     wayland-122 = pkgs.wayland.overrideAttrs (oldAttrs: {
+  #       version = "1.22.0";
+  #       src = pkgs.fetchurl {
+  #         url = "https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.22.0/downloads/wayland-1.22.0.tar.xz";
+  #         hash = "sha256-FUCvHqaYpHHC2OnSiDMsfg/TYMjx0Sk267fny8JCWEI=";
+  #       };
+  #     });
+  #     mesa-rebuilt = pkgs.mesa.override { wayland = wayland-122; };
+  #   in
+  #   (builtins.map
+  #     (output: {
+  #       original = pkgs.wayland.${output};
+  #       replacement = wayland-122.${output};
+  #     }) [ "out" "bin" "dev" ])
+  #   ++ (builtins.map
+  #     (output: {
+  #       original = pkgs.mesa.${output};
+  #       replacement = mesa-rebuilt.${output};
+  #     }) [ "out" "dev" "drivers" ]);
 }
