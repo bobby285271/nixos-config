f66a5590e4dc1efea542f259367ebb5b32cc1312
Bobby Rong <rjl931189261@126.com>
2023-04-24 21:56:39 +0800

    desktop: install dingtalk

--- desktop/packages.nix
+++ desktop/packages.nix
@@ -2,10 +2,12 @@
 
 let
   libreoffice-icon-fixed = pkgs.callPackage ./pkgs/libreoffice-icon-fixed.nix { };
+  dingtalk = pkgs.callPackage ./pkgs/dingtalk { };
 in
 {
   environment.systemPackages = with pkgs; [
     chromium
+    dingtalk
     drawing
     firefox
     gitg
