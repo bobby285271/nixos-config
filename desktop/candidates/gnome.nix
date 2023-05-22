a66f90b7b7db52692dc8269db227ac789b88e271
Bobby Rong <rjl931189261@126.com>
2023-05-22 14:31:03 +0800

    desktop: add temp gtk4 hack

--- desktop/candidates/gnome.nix
+++ desktop/candidates/gnome.nix
@@ -15,34 +15,24 @@
   programs.gpaste.enable = true;
   xdg.portal.enable = true;
 
-  # system.replaceRuntimeDependencies =
-  #   let
-  #     gtk4-10-3 = pkgs.gtk4.overrideAttrs (oldAttrs: {
-  #       version = "4.10.3";
-  #       src = pkgs.fetchurl {
-  #         url = "mirror://gnome/sources/gtk/4.10/gtk-4.10.3.tar.xz";
-  #         sha256 = "sha256-RUVEGteeN3624KcFAm3HpGiG5GobA020CRKQnagBzqk=";
-  #       };
-  #     });
-  #     libadwaita1-3-2 = pkgs.libadwaita.overrideAttrs (oldAttrs: {
-  #       version = "1.3.2";
-  #       src = pkgs.fetchFromGitLab {
-  #         domain = "gitlab.gnome.org";
-  #         owner = "GNOME";
-  #         repo = "libadwaita";
-  #         rev = "1.3.2";
-  #         hash = "sha256-9Qha8xN3lC/t5dQNYPbgMX6HAKgEk80pyycrd5MGYLo=";
-  #       };
-  #     });
-  #   in
-  #   (builtins.map
-  #     (output: {
-  #       original = pkgs.gtk4.${output};
-  #       replacement = gtk4-10-3.${output};
-  #     }) [ "out" /* "dev" "devdoc" */ ])
-  #   ++ (builtins.map
-  #     (output: {
-  #       original = pkgs.libadwaita.${output};
-  #       replacement = libadwaita1-3-2.${output};
-  #     }) [ "out" /* "dev" "devdoc" */ ]);
+  system.replaceRuntimeDependencies =
+    let
+      gtk4-10-3 = pkgs.gtk4.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [
+          (pkgs.fetchpatch2 {
+            url = "https://gitlab.gnome.org/GNOME/gtk/-/commit/4f47683710bbb4b56c286c6ee6a5c394fcf2b755.patch";
+            sha256 = "fU9SX8MH37ZN6Ffk/YhYmipTC7+uT9JXnWggArWNkqA=";
+          })
+          (pkgs.fetchpatch2 {
+            url = "https://gitlab.gnome.org/GNOME/gtk/-/commit/b686ce1cb62dba505120e0f1116c516662a06e30.patch";
+            sha256 = "0zjY5s+T4CVe3WiowgWE58ruVvqBFUuY2juwBOzMRN4=";
+          })
+        ];
+      });
+    in
+    (builtins.map
+      (output: {
+        original = pkgs.gtk4.${output};
+        replacement = gtk4-10-3.${output};
+      }) [ "out" /* "dev" "devdoc" */ ]);
 }
