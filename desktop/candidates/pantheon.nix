56cb8f7528801fc6eee6625a2823baa652c149e1
Bobby Rong <rjl931189261@126.com>
2023-05-25 21:26:53 +0800

    pantheon: re-add wingpanel-indicator-ayatana

--- desktop/candidates/pantheon.nix
+++ desktop/candidates/pantheon.nix
@@ -24,6 +24,17 @@
     ];
   };
 
+  services.xserver.desktopManager.pantheon.extraWingpanelIndicators = with pkgs; [ wingpanel-indicator-ayatana ];
+
+  systemd.user.services.indicatorapp = {
+    description = "indicator-application-gtk3";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.indicator-application-gtk3}/libexec/indicator-application/indicator-application-service";
+    };
+  };
+
   nixpkgs.overlays = [
     (self: super: {
       pantheon = super.pantheon.overrideScope' (pself: psuper: {
