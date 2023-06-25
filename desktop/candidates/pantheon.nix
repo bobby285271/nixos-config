536039e2deee0e0b9cd80854130695fa4e1b02c8
Bobby Rong <rjl931189261@126.com>
2023-06-25 10:24:53 +0800

    pantheon: disable wingpanel-indicator-ayatana

--- desktop/candidates/pantheon.nix
+++ desktop/candidates/pantheon.nix
@@ -24,16 +24,16 @@
     ];
   };
 
-  services.xserver.desktopManager.pantheon.extraWingpanelIndicators = with pkgs; [ wingpanel-indicator-ayatana ];
+  # services.xserver.desktopManager.pantheon.extraWingpanelIndicators = with pkgs; [ wingpanel-indicator-ayatana ];
 
-  systemd.user.services.indicatorapp = {
-    description = "indicator-application-gtk3";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.indicator-application-gtk3}/libexec/indicator-application/indicator-application-service";
-    };
-  };
+  # systemd.user.services.indicatorapp = {
+  #   description = "indicator-application-gtk3";
+  #   wantedBy = [ "graphical-session.target" ];
+  #   partOf = [ "graphical-session.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.indicator-application-gtk3}/libexec/indicator-application/indicator-application-service";
+  #   };
+  # };
 
   nixpkgs.overlays = [
     (self: super: {
