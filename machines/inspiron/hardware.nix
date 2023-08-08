3e69d984e7e63b6c0338efd5d1620cc23a9969a0
Bobby Rong <rjl931189261@126.com>
2023-08-08 10:31:57 +0800

    hardware: enable zramSwap

--- machines/inspiron/hardware.nix
+++ machines/inspiron/hardware.nix
@@ -58,4 +58,6 @@
   networking.useDHCP = lib.mkDefault true;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  zramSwap.enable = true;
 }
