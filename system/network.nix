a61fe7f7539ad4a06d063fe64cde7b74edab038d
Bobby Rong <rjl931189261@126.com>
2023-08-20 21:40:55 +0800

    network: update gh host

--- system/network.nix
+++ system/network.nix
@@ -24,25 +24,26 @@
     ];
     hosts = {
       # GitHub
-      "13.107.238.51" = [ "vscode.dev" ];
+      "13.107.253.40" = [ "vscode.dev" ];
       "13.107.42.16" = [ "pipelines.actions.githubusercontent.com" ];
-      "140.82.112.26" = [
-        "alive.github.com"
-        "live.github.com"
-      ];
+      "140.82.112.18" = [ "github.community" ];
+      "140.82.112.21" = [ "collector.github.com" ];
+      "140.82.112.26" = [ "alive.github.com" ];
+      "140.82.112.3" = [ "github.com" ];
       "140.82.112.9" = [ "codeload.github.com" ];
-      "140.82.113.21" = [
-        "central.github.com"
-        "collector.github.com"
+      "140.82.113.21" = [ "central.github.com" ];
+      "140.82.113.4" = [ "gist.github.com" ];
+      "140.82.113.5" = [ "api.github.com" ];
+      "140.82.114.21" = [ "education.github.com" ];
+      "140.82.114.25" = [ "live.github.com" ];
+      "151.101.1.194" = [ "github.global.ssl.fastly.net" ];
+      "185.199.110.153" = [
+        "assets-cdn.github.com"
+        "github.io"
+        "githubstatus.com"
       ];
-      "140.82.114.17" = [ "github.community" ];
-      "140.82.114.3" = [
-        "gist.github.com"
-        "github.com"
-      ];
-      "140.82.114.6" = [ "api.github.com" ];
-      "151.101.65.194" = [ "github.global.ssl.fastly.net" ];
-      "185.199.108.133" = [
+      "185.199.110.154" = [ "github.githubassets.com" ];
+      "185.199.111.133" = [
         "avatars.githubusercontent.com"
         "avatars0.githubusercontent.com"
         "avatars1.githubusercontent.com"
@@ -58,20 +59,14 @@
         "media.githubusercontent.com"
         "objects.githubusercontent.com"
         "raw.githubusercontent.com"
+        "user-images.githubusercontent.com"
       ];
-      "185.199.109.154" = [ "github.githubassets.com" ];
-      "185.199.110.153" = [
-        "assets-cdn.github.com"
-        "github.io"
-        "githubstatus.com"
-      ];
-      "185.199.111.133" = [ "user-images.githubusercontent.com" ];
       "192.0.66.2" = [ "github.blog" ];
-      "52.217.141.73" = [ "github-production-user-asset-6210df.s3.amazonaws.com" ];
-      "52.217.206.193" = [ "github-production-repository-file-5c1aeb.s3.amazonaws.com" ];
-      "52.217.234.145" = [ "github-com.s3.amazonaws.com" ];
-      "54.231.132.193" = [ "github-cloud.s3.amazonaws.com" ];
-      "54.231.139.249" = [ "github-production-release-asset-2e65be.s3.amazonaws.com" ];
+      "52.216.210.129" = [ "github-production-release-asset-2e65be.s3.amazonaws.com" ];
+      "52.216.216.209" = [ "github-production-repository-file-5c1aeb.s3.amazonaws.com" ];
+      "52.217.201.217" = [ "github-cloud.s3.amazonaws.com" ];
+      "52.217.71.76" = [ "github-com.s3.amazonaws.com" ];
+      "54.231.132.97" = [ "github-production-user-asset-6210df.s3.amazonaws.com" ];
       # Cachix
       "104.26.13.82" = [ "nix-community.cachix.org" ];
     };
