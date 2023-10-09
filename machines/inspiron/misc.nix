9d0549cf9bd59022705bb883af12f3b09a921d5e
Bobby Rong <rjl931189261@126.com>
2023-10-10 00:15:37 +0800

    misc: clean up

--- machines/inspiron/misc.nix
+++ machines/inspiron/misc.nix
@@ -1,16 +1,6 @@
 { pkgs, ... }:
 
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
-in
 {
   networking.hostName = "inspiron";
-  environment.systemPackages = [ nvidia-offload ];
   zramSwap.enable = true;
 }
