a7936171f73a87560ba14aa5bc2da02269fc0a15
Bobby Rong <rjl931189261@126.com>
2023-10-11 09:00:38 +0800

    boot: re-add nvidia

--- machines/inspiron/misc.nix
+++ machines/inspiron/misc.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in
 {
+  environment.systemPackages = [ nvidia-offload ];
   networking.hostName = "inspiron";
   zramSwap.enable = true;
 }
