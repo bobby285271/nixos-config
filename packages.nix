32c9b76c0293bdcea8a1387bfa2710a42d0e7823
Bobby Rong <rjl931189261@126.com>
2023-10-26 18:58:32 +0800

    pkgs: add clash-for-windows

--- packages.nix
+++ packages.nix
@@ -1,5 +1,6 @@
 { pkgs ? import <nixpkgs> { } }: {
   # Desktop
+  clash-for-windows = pkgs.callPackage ./desktop/pkgs/clash-for-windows.nix { };
   libreoffice-icon-fixed = pkgs.callPackage ./desktop/pkgs/libreoffice-icon-fixed.nix { };
   dingtalk = pkgs.callPackage ./desktop/pkgs/dingtalk { };
   mate-menu = pkgs.callPackage ./desktop/pkgs/mate-menu.nix { };
