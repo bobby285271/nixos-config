b8f9d39cabdd8267a26ce860546a082fa73de8a9
Bobby Rong <rjl931189261@126.com>
2023-11-05 10:18:24 +0800

    misc: use enableOffloadCmd

--- machines/inspiron/misc.nix
+++ machines/inspiron/misc.nix
@@ -1,16 +1,7 @@
 { pkgs, ... }:
 
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
-in
 {
-  environment.systemPackages = [ nvidia-offload ];
+  hardware.nvidia.prime.offload.enableOffloadCmd = true;
   networking.hostName = "inspiron";
   zramSwap.enable = true;
 }
