96933d092203767dfb163332ffd3a88066c58ce9
Bobby Rong <rjl931189261@126.com>
2023-11-05 10:48:23 +0800

    inspiron: add another script

--- machines/inspiron/scripts.nix
+++ machines/inspiron/scripts.nix
@@ -11,7 +11,13 @@ let
   ];
 in
 {
-  environment.systemPackages = (map
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "review.sh" ''
+      cd /home/bobby285271/nixpkgs
+      git fetch https://hub.fgit.cf/nixos/nixpkgs pull/''${1}/head
+      git checkout FETCH_HEAD
+    '')
+  ] ++ (map
     (desktop: pkgs.writeShellScriptBin "nixos-${desktop}.sh" ''
       /home/bobby285271/nixos/helper/__internal-update-common.sh "inspiron${lib.optionalString (desktop != "cinnamon") "-${desktop}"}" "$@"
     '')
