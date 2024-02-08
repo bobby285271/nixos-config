c33639847feed164363a33c1f7ec54fa2a9905d5
Bobby Rong <rjl931189261@126.com>
2024-02-08 18:15:06 +0800

    scripts: add nonemast.sh

--- machines/inspiron/scripts.nix
+++ machines/inspiron/scripts.nix
@@ -17,6 +17,10 @@ in
       git fetch ssh://git@ssh.github.com:443/nixos/nixpkgs pull/''${1}/head
       git checkout FETCH_HEAD
     '')
+    (pkgs.writeShellScriptBin "nonemast.sh" ''
+      nix run /home/bobby285271/nonemast --override-input nixpkgs \
+        'git+file:///home/bobby285271/nixpkgs?ref=nixos-unstable' -- "$@"
+    '')
   ] ++ (map
     (desktop: pkgs.writeShellScriptBin "nixos-${desktop}.sh" ''
       /home/bobby285271/nixos/helper/__internal-update-common.sh "inspiron-${desktop}" "$@"
