3e2b834dbb9ead25a1549834b73a09e7cb9a128b
Bobby Rong <rjl931189261@126.com>
2024-08-25 16:07:54 +0800

    nixconfig: try latest

--- system/nixconfig.nix
+++ system/nixconfig.nix
@@ -1,22 +1,25 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    auto-optimise-store = true;
-    flake-registry = "https://jihulab.com/bobby285271/flake-registry/-/raw/master/flake-registry.json";
-    # substituters = lib.mkForce [
-    substituters = [
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      # "https://bobby285271.cachix.org"
-    ];
-    # trusted-public-keys = [
-    #   "bobby285271.cachix.org-1:suZrwf/wphZbNXocjgcc49lcn/RB0iFxlh1peSVOE3w="
-    # ];
+  nix = {
+    package = pkgs.nixVersions.latest;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      auto-optimise-store = true;
+      flake-registry = "https://jihulab.com/bobby285271/flake-registry/-/raw/master/flake-registry.json";
+      # substituters = lib.mkForce [
+      substituters = [
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # "https://bobby285271.cachix.org"
+      ];
+      # trusted-public-keys = [
+      #   "bobby285271.cachix.org-1:suZrwf/wphZbNXocjgcc49lcn/RB0iFxlh1peSVOE3w="
+      # ];
+    };
   };
 
   nixpkgs.config = {
