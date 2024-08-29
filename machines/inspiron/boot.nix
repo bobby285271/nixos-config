19a0db1cf31cc414902ca4e8a82d0f710aada8e7
Bobby Rong <rjl931189261@126.com>
2024-08-30 00:20:08 +0800

    boot: disable open

--- machines/inspiron/boot.nix
+++ machines/inspiron/boot.nix
@@ -17,13 +17,16 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
+  hardware.nvidia = {
+    open = false;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
     };
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
   };
 
   networking.hostName = "inspiron";
