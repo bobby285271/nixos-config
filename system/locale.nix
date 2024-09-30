8ffa643e942922e59ad074ee33d0d883dc1bbcc5
Bobby Rong <rjl931189261@126.com>
2024-09-30 16:59:10 +0800

    locale: add ja_JP for testing

--- system/locale.nix
+++ system/locale.nix
@@ -1,9 +1,10 @@
 {
   i18n = {
-    defaultLocale = "zh_CN.UTF-8";
+    defaultLocale = "ja_JP.UTF-8";
     supportedLocales = [
       "zh_CN.UTF-8/UTF-8"
       "en_US.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
     ];
   };
 
