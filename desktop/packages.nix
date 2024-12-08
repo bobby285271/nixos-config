956e4a1d1f9fe23b6e6c6243391b979f9b9a8178
Bobby Rong <rjl931189261@126.com>
2024-12-08 12:39:46 +0800

    desktop: start testing gtk4 mr 7836
    Noticed in https://github.com/NixOS/nixpkgs/pull/362830.

--- desktop/packages.nix
+++ desktop/packages.nix
@@ -18,5 +18,30 @@ in
     thunderbird
     vscode-fhs
     # vscodium-fhs
+    timeshift
   ];
+
+  system.replaceDependencies.replacements =
+    let
+      # Report all the crashes *downstream* https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/7836
+      gtk4-test = pkgs.gtk4.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/7836.diff";
+            hash = "sha256-AwsqnjzkHW0qf/7PUUjrUAUIwmbeq2pvoq1LhCu7tw0=";
+          })
+        ];
+      });
+    in
+    (builtins.map
+      (output: {
+        original = pkgs.gtk4.${output};
+        replacement = gtk4-test.${output};
+      })
+      [
+        "out"
+        "dev"
+        "devdoc"
+      ]
+    );
 }
