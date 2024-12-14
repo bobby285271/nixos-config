45a6a2432e76aca0baabdff30f64f16d19785cc0
Bobby Rong <rjl931189261@126.com>
2024-12-14 20:53:53 +0800

    desktop: comment out the gtk4 testing

--- desktop/packages.nix
+++ desktop/packages.nix
@@ -21,27 +21,27 @@ in
     timeshift
   ];
 
-  system.replaceDependencies.replacements =
-    let
-      # Report all the crashes *downstream* https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/7836
-      gtk4-test = pkgs.gtk4.overrideAttrs (oldAttrs: {
-        patches = (oldAttrs.patches or [ ]) ++ [
-          (pkgs.fetchpatch {
-            url = "https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/7836.diff";
-            hash = "sha256-AwsqnjzkHW0qf/7PUUjrUAUIwmbeq2pvoq1LhCu7tw0=";
-          })
-        ];
-      });
-    in
-    (builtins.map
-      (output: {
-        original = pkgs.gtk4.${output};
-        replacement = gtk4-test.${output};
-      })
-      [
-        "out"
-        "dev"
-        "devdoc"
-      ]
-    );
+  # system.replaceDependencies.replacements =
+  #   let
+  #     # Report all the crashes *downstream* https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/7836
+  #     gtk4-test = pkgs.gtk4.overrideAttrs (oldAttrs: {
+  #       patches = (oldAttrs.patches or [ ]) ++ [
+  #         (pkgs.fetchpatch {
+  #           url = "https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/7836.diff";
+  #           hash = "sha256-AwsqnjzkHW0qf/7PUUjrUAUIwmbeq2pvoq1LhCu7tw0=";
+  #         })
+  #       ];
+  #     });
+  #   in
+  #   (builtins.map
+  #     (output: {
+  #       original = pkgs.gtk4.${output};
+  #       replacement = gtk4-test.${output};
+  #     })
+  #     [
+  #       "out"
+  #       "dev"
+  #       "devdoc"
+  #     ]
+  #   );
 }
