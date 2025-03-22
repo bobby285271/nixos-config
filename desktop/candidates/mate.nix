be3e97e228cfc92da93a491fd56d8e5132f2dd08
Bobby Rong <rjl931189261@126.com>
2025-03-22 22:28:31 +0800

    mate: revert glib test

--- desktop/candidates/mate.nix
+++ desktop/candidates/mate.nix
@@ -10,6 +10,7 @@
     };
     flatpak.enable = true;
     accounts-daemon.enable = true;
+    udisks2.enable = true;
   };
 
   programs.wayfire.plugins = with pkgs.wayfirePlugins; [
@@ -56,28 +57,29 @@
     ];
   };
 
-  system.replaceDependencies.replacements =
-    let
-      glib_next = pkgs.glib.overrideAttrs (_: {
-        version = "2.84.0";
-        src = pkgs.fetchurl {
-          url = "mirror://gnome/sources/glib/2.84/glib-2.84.0.tar.xz";
-          hash = "sha256-+II2AMuFQl4oFc+tguog/apThIKrdOcpPViz9kpa/2o=";
-        };
-      });
-    in
-    (
-      (builtins.map
-        (output: {
-          original = pkgs.glib.${output};
-          replacement = glib_next.${output};
-        })
-        [
-          "out"
-          "dev"
-        ]
-      )
-    );
+  # system.replaceDependencies.replacements =
+  #   let
+  #     glib_next = pkgs.glib.overrideAttrs (_: {
+  #       version = "2.84.0";
+  #       src = pkgs.fetchurl {
+  #         url = "mirror://gnome/sources/glib/2.84/glib-2.84.0.tar.xz";
+  #         hash = "sha256-+II2AMuFQl4oFc+tguog/apThIKrdOcpPViz9kpa/2o=";
+  #       };
+  #     });
+  #   in
+  #   (
+  #     (builtins.map
+  #       (output: {
+  #         original = pkgs.glib.${output};
+  #         replacement = glib_next.${output};
+  #       })
+  #       [
+  #         "bin"
+  #         "out"
+  #         "dev"
+  #       ]
+  #     )
+  #   );
 
   environment.systemPackages = with pkgs; [
     # elementary-xfce-icon-theme
