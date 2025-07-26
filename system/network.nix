62605c232ec9e71c00a5e5434ff0683cf24b959a
Bobby Rong <rjl931189261@126.com>
2025-07-26 11:23:50 +0800

    network: update github hosts

--- system/network.nix
+++ system/network.nix
@@ -34,26 +34,21 @@
     # ];
     hosts = {
       # GitHub
-      "13.107.253.40" = [ "vscode.dev" ];
+      "13.107.246.73" = [ "vscode.dev" ];
       "13.107.42.16" = [ "pipelines.actions.githubusercontent.com" ];
-      "140.82.112.18" = [ "github.community" ];
-      "140.82.112.21" = [ "collector.github.com" ];
-      "140.82.112.26" = [ "alive.github.com" ];
-      "140.82.112.3" = [ "github.com" ];
-      "140.82.112.9" = [ "codeload.github.com" ];
-      "140.82.113.21" = [ "central.github.com" ];
-      "140.82.113.4" = [ "gist.github.com" ];
-      "140.82.113.5" = [ "api.github.com" ];
-      "140.82.114.21" = [ "education.github.com" ];
-      "140.82.114.25" = [ "live.github.com" ];
-      "151.101.1.194" = [ "github.global.ssl.fastly.net" ];
-      "185.199.110.153" = [
-        "assets-cdn.github.com"
-        "github.io"
-        "githubstatus.com"
+      "140.82.112.21" = [ "api.individual.githubcopilot.com" ];
+      "140.82.113.18" = [ "github.community" ];
+      "140.82.113.21" = [
+        "central.github.com"
+        "collector.github.com"
+        "education.github.com"
       ];
-      "185.199.110.154" = [ "github.githubassets.com" ];
-      "185.199.111.133" = [
+      "140.82.113.25" = [ "live.github.com" ];
+      "140.82.113.26" = [ "alive.github.com" ];
+      "151.101.193.194" = [ "github.global.ssl.fastly.net" ];
+      "16.182.37.121" = [ "github-cloud.s3.amazonaws.com" ];
+      "16.182.99.185" = [ "github-com.s3.amazonaws.com" ];
+      "185.199.110.133" = [
         "avatars.githubusercontent.com"
         "avatars0.githubusercontent.com"
         "avatars1.githubusercontent.com"
@@ -70,13 +65,23 @@
         "objects.githubusercontent.com"
         "raw.githubusercontent.com"
         "user-images.githubusercontent.com"
+        "private-user-images.githubusercontent.com"
       ];
+      "185.199.111.153" = [
+        "github.io"
+        "githubstatus.com"
+      ];
+      "185.199.111.154" = [ "github.githubassets.com" ];
       "192.0.66.2" = [ "github.blog" ];
-      "52.216.210.129" = [ "github-production-release-asset-2e65be.s3.amazonaws.com" ];
-      "52.216.216.209" = [ "github-production-repository-file-5c1aeb.s3.amazonaws.com" ];
-      "52.217.201.217" = [ "github-cloud.s3.amazonaws.com" ];
-      "52.217.71.76" = [ "github-com.s3.amazonaws.com" ];
-      "54.231.132.97" = [ "github-production-user-asset-6210df.s3.amazonaws.com" ];
+      "20.205.243.165" = [ "codeload.github.com" ];
+      "20.205.243.166" = [
+        "gist.github.com"
+        "github.com"
+      ];
+      "20.205.243.168" = [ "api.github.com" ];
+      "3.5.0.129" = [ "github-production-user-asset-6210df.s3.amazonaws.com" ];
+      "3.5.2.206" = [ "github-production-release-asset-2e65be.s3.amazonaws.com" ];
+      "52.216.186.139" = [ "github-production-repository-file-5c1aeb.s3.amazonaws.com" ];
       # Cachix
       "104.26.13.82" = [ "nix-community.cachix.org" ];
       # ????
