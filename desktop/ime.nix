8afec393c2cc1e8700871a4d4aae2208cda7d49d
Bobby Rong <rjl931189261@126.com>
2025-08-26 09:10:11 +0800

    ime: use qt6 fcitx

--- desktop/ime.nix
+++ desktop/ime.nix
@@ -3,7 +3,7 @@
 #   i18n.inputMethod = {
 #     enable = true;
 #     type = "fcitx5";
-#     fcitx5.addons = [ pkgs.libsForQt5.fcitx5-chinese-addons ];
+#     fcitx5.addons = [ pkgs.qt6Packages.fcitx5-chinese-addons ];
 #     # fcitx5.addons = [ pkgs.fcitx5-chinese-addons ];
 #   };
 # }
@@ -23,7 +23,8 @@ in
     enable = true;
     type = if preferIbus then "ibus" else "fcitx5";
     fcitx5 = lib.mkIf (!preferIbus) {
-      addons = [ pkgs.fcitx5-chinese-addons ];
+      addons = [ pkgs.qt6Packages.fcitx5-chinese-addons ];
+      plasma6Support = true;
     };
     ibus = lib.mkIf preferIbus {
       engines = [ pkgs.ibus-engines.libpinyin ];
