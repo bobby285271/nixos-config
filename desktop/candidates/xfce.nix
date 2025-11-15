13d3a4f390fd9c3cba4945553f018e7c804c213e
Bobby Rong <rjl931189261@126.com>
2025-11-15 23:05:54 +0800

    xfce: use greybird-dark in greetd

--- desktop/candidates/xfce.nix
+++ desktop/candidates/xfce.nix
@@ -4,7 +4,7 @@
   services = {
     greetd = {
       enable = true;
-      settings.default_session.command = "${pkgs.cage}/bin/cage -s -- sh -c 'wlr-randr --output eDP-1 --scale 2 && GTK_THEME=Greybird ${pkgs.gtkgreet}/bin/gtkgreet'";
+      settings.default_session.command = "${pkgs.cage}/bin/cage -s -- sh -c 'wlr-randr --output eDP-1 --scale 2 && GTK_THEME=Greybird-dark ${pkgs.gtkgreet}/bin/gtkgreet'";
     };
 
     xserver = {
