75848767fe7ba06f1771ec4856198c30b0b01ebf
Bobby Rong <rjl931189261@126.com>
2026-03-02 20:36:09 +0800

    helper: upd

--- helper/nix-daemon-proxy.sh
+++ helper/nix-daemon-proxy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# sudo mkdir /run/systemd/system/nix-daemon.service.d/
-# sudo echo "[Service]" > /run/systemd/system/nix-daemon.service.d/override.conf
-# sudo echo 'Environment="http_proxy=http://localhost:20172"' >> /run/systemd/system/nix-daemon.service.d/override.conf
-# sudo echo 'Environment="https_proxy=http://localhost:20172"' >> /run/systemd/system/nix-daemon.service.d/override.conf
-# sudo echo 'Environment="all_proxy=http://localhost:20172"' >> /run/systemd/system/nix-daemon.service.d/override.conf
-# sudo systemctl daemon-reload
-# sudo systemctl restart nix-daemon
+exit 0
+
+sudo mkdir /run/systemd/system/nix-daemon.service.d/
+sudo echo "[Service]" > /run/systemd/system/nix-daemon.service.d/override.conf
+sudo echo 'Environment="http_proxy=http://localhost:20172"' >> /run/systemd/system/nix-daemon.service.d/override.conf
+sudo echo 'Environment="https_proxy=http://localhost:20172"' >> /run/systemd/system/nix-daemon.service.d/override.conf
+sudo echo 'Environment="all_proxy=http://localhost:20172"' >> /run/systemd/system/nix-daemon.service.d/override.conf
+sudo systemctl daemon-reload
+sudo systemctl restart nix-daemon
